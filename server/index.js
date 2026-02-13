0a35ba6e6957eeb2377bb28396ae8aacd34c46d5
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-02-13 17:11:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://scholr-xpe9.onrender.com", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
