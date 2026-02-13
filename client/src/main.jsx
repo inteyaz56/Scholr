812ab68d839192528eacaa2d18d33243270f4375
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-02-13 17:05:02 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://scholr-backend-prsx.onrender.com";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
