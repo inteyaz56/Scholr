93d6abd0264341c9afc0bb40d8ddd0b0eaa4e56f
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-02-13 17:15:34 +0530

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -73,8 +73,8 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
